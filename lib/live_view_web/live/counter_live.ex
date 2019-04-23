3674e132cfb7dbb88995376e9e0789faaafe7bac
Thomas Johnson <tjohnson1106@gmail.com>
2019-04-23 16:36:19 -0400

    created handle event functions

--- lib/live_view_web/live/counter_live.ex
+++ lib/live_view_web/live/counter_live.ex
@@ -18,4 +18,12 @@ defmodule LiveViewWeb.CounterLive do
   def mount(_session, socket) do
     {:ok, assign(socket, :val, 0)}
   end
+
+  def handle_event("inc", _value, socket) do
+    {:noreply, update(socket, :val, &(&1 + 1))}
+  end
+
+  def handle_event("dec", _value, socket) do
+    {:noreply, update(socket, :val, &(&1 - 1))}
+  end
 end
